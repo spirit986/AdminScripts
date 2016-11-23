010e3e2bfb989428203380af0a83b5178ed37a08
spirit986 <spirit.986@gmail.com>
2016-11-23 14:48:07 +0100

    Description banner on top of the script

--- rnw.sh
+++ rnw.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##############################################################################
+# The command `grep -rnw /path/to/dir -e "pattern"`
+# is the one of the commands that I use very often so I created a script
+# that will ease it for me
+# 
+# This is provided as is, without any warranties.
+# Use it at your own risk.
+##############################################################################
+
 if [ -z ${1+x} ]; 
  then 
  echo "LOCATION is not set! \n\nUsage:\nrnw [LOCATION] [PATTERN]\n\n";
