9c29227213806312ac80b0c5beb73d70ca9cae9a
Tom <spirit.986@gmail.com>
2019-09-19 12:49:57 +0200

    Create hot_add_memory.sh

---
+++ hot_add_memory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Bring all new Memory online
+for RAM in $(grep line /sys/devices/system/memory/*/state)
+do
+    echo "Found ram: ${RAM} ..."
+    if [[ "${RAM}" == *":offline" ]]; then
+        echo "Bringing online"
+        echo $RAM | sed "s/:offline$//"|sed "s/^/echo online > /"|source /dev/stdin
+    else
+        echo "Already online"
+    fi
+done
